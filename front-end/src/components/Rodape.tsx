79399fff80e156853c4098b757d845eae75618f9
phestalise <phestalise@gmail.com>
2025-10-06 13:53:23 -0300

    adicionar componente Rodape

---
+++ front-end/src/components/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Rodape() {
+  return (
+    <footer className="py-4 text-center text-sm text-gray-600 border-t mt-6">
+      Desenvolvido por: Nome do Grupo - RM: 000000
+    </footer>
+  );
+}

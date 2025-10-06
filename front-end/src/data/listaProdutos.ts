76ea5cd893ab997b1d60ef07c0f40075cd674fc5
phestalise <phestalise@gmail.com>
2025-10-06 13:57:09 -0300

    adicionando a lista com dados de exemplo

---
+++ front-end/src/data/listaProdutos.ts
@@ -0,0 +1,9 @@
+export type TipoProduto = { id: number; nome: string; preco: number };
+
+export const listaProdutos: TipoProduto[] = [
+  { id: 1, nome: "Teclado", preco: 150 },
+  { id: 2, nome: "Mouse", preco: 100 },
+  { id: 3, nome: "Mouse Pad", preco: 50 },
+  { id: 4, nome: "Cadeira Gamer", preco: 2570 },
+  { id: 5, nome: "Monitor 32'", preco: 3869 }
+];

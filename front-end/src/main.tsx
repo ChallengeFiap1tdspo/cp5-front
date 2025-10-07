bfbc1c148e2760bb0907e052d708d9dc744bb053
phestalise <phestalise@gmail.com>
2025-10-06 23:56:00 -0300

    adicionando a home

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -4,18 +4,19 @@ import App from "./App";
 import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Login from "./routes/Login";
 import Cadastro from "./routes/Cadastro";
+import Home from "./routes/Home"; // <-- adicione isso
 import { AuthProvider } from "./contexts/AuthContext";
 import "./global.css";
-
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "/", element: <Navigate to="/login" /> }, 
+      { path: "/", element: <Navigate to="/login" /> },
       { path: "/login", element: <Login /> },
       { path: "/cadastro", element: <Cadastro /> },
+      { path: "/home", element: <Home /> }, 
     ],
   },
 ]);
